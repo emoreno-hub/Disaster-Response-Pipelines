758d599fd012bc0bb20a23d380d60ecbc4eab203
emoreno-hub <69337336+emoreno-hub@users.noreply.github.com>
2021-08-02 08:50:38 -0700

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -68,7 +68,7 @@ def save_data(df, database_filename):
     '''
     Save data for SQLite database as .db file
     '''
-    engine = create_engine('sqlite:///DisasterResponse.db')
+    engine = create_engine('sqlite:///' + database_filename)
     df.to_sql('Messages', engine, index=False)
     
     pass
