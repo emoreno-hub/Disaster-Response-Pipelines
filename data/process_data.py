c216bb7ba7cff88b5efc10855c7522fca95bf64e
emoreno-hub <69337336+emoreno-hub@users.noreply.github.com>
2021-08-02 15:55:15 -0700

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -69,7 +69,7 @@ def save_data(df, database_filename):
     Save data for SQLite database as .db file
     '''
     engine = create_engine('sqlite:///' + database_filename)
-    df.to_sql('Messages', engine, index=False)
+    df.to_sql('Messages', engine, index=False, if_exists='replace)
     
     pass
 
